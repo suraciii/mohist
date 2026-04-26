d0e967311aa779dc63a0caa8a32aee534ad6c0ef
Tequila Sunset <suraciii@outlook.com>
2026-04-26 11:36:03 +0800

    T-002: add checkpointRepo to WorkflowController and thread through executePipeline

--- packages/cli/src/server/index.ts
+++ packages/cli/src/server/index.ts
@@ -105,7 +105,7 @@ async function main(): Promise<void> {
   const sessionManager = new SessionManager();
   const eventBus = new EventBus();
   const workflowLogRepo = stateManager.getWorkflowLogRepo();
-  const agentRunner = new AgentRunnerService(eventBus, workflowLogRepo, stateManager.getIssueRepo(), configService.getMaxConcurrentAgents(), stateManager.getAgentSessionMessageRepo(), stateManager.getCoderSessionRepo());
+  const agentRunner = new AgentRunnerService(eventBus, workflowLogRepo, stateManager.getIssueRepo(), configService.getMaxConcurrentAgents(), stateManager.getAgentSessionMessageRepo(), stateManager.getCoderSessionRepo(), stateManager.getPipelineCheckpointRepo());
 
   agentRunner.setLlmConfig(fileConfig);
 
