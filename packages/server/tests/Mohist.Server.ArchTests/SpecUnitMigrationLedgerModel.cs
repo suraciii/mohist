0b649cf028aef53d5de49074de95bb9c2ff6fb5e
Tequila Sunset <suraciii@outlook.com>
2026-08-11 15:32:08 +0800

    test(arch): refresh integrated spec source ledger

--- packages/server/tests/Mohist.Server.ArchTests/SpecUnitMigrationLedgerModel.cs
+++ packages/server/tests/Mohist.Server.ArchTests/SpecUnitMigrationLedgerModel.cs
@@ -6,8 +6,8 @@ internal static class SpecUnitMigrationLedgerValidator
 {
     internal const string ValidationHead = "2c96e43e2bc89fcfbd4e051576faec8f2861a8a8";
     internal const string ValidationTree = "ff159609b97df5b1fac6d6404a6d1811f6bda99a";
-    internal const int ValidationSourceTreeFileCount = 830;
-    internal const string ValidationSourceTreeDigest = "d385f1c9d1d32fea83c11f8b671409e386ed2f82ca8de0da32e29000d418d8ea";
+    internal const int ValidationSourceTreeFileCount = 831;
+    internal const string ValidationSourceTreeDigest = "be7502a1a54712a4599cf693a5d1956bd402a8b4bdb283e3d293bf4653dcaa5b";
     internal const string ExpectedValidationBaselineDigest = "03cad8aca4e8ddf9688d7b3532d03cb1b93c039e8a9ec4e0e38ee73e5b403c62";
     internal const string Pr388Commit = "602efa6abd6fca3efcd43b66b47ba10a80d9faba";
     internal const string Pr388Parent = "0f723ddb87dfd0943b32e5b53b9af9ccbc89367d";
