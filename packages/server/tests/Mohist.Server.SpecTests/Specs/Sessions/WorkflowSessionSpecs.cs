fa9ebd8ce23523c0c1fae99dbc440055c043c186
Tequila Sunset <suraciii@outlook.com>
2026-08-26 12:26:58 +0800

    test: relocate HTTP-layer workflow specs out of Grain folder (#681 follow-up)

--- packages/server/tests/Mohist.Server.SpecTests/Specs/Sessions/WorkflowSessionSpecs.cs
+++ packages/server/tests/Mohist.Server.SpecTests/Specs/Sessions/WorkflowSessionSpecs.cs
@@ -9,12 +9,13 @@ using Mohist.Server.Infrastructure.Events;
 using Mohist.Server.Issue.Grains;
 using Mohist.Server.Sessions.Domain;
 using Mohist.Server.Sessions.Grains;
+using Mohist.Server.SpecTests.Specs.Workflow;
 using Mohist.Server.SpecTests.Support;
 using Mohist.Server.TestSupport;
 using Mohist.Server.Sessions.Services;
 using Xunit;
 
-namespace Mohist.Server.SpecTests.Specs.Workflow.Grain;
+namespace Mohist.Server.SpecTests.Specs.Sessions;
 
 public class WorkflowSessionSpecs
 {
